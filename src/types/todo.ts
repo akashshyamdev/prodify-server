4196f132f049980287f2010a2f89522c9efd73d1
Akash Shyam <akash.shyam2008@gmail.com>
2021-09-28 18:48:41 +0530

    types(todo): updated types

--- src/types/todo.ts
+++ src/types/todo.ts
@@ -1,5 +1,10 @@
 import { Document } from 'mongoose';
+import { IDeveloper } from './developer';
 
 export interface ITodo extends Document {
   _id: string;
+  title: string;
+  due: Date;
+  isCompleted: boolean;
+  developer: IDeveloper;
 }
