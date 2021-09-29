0cf79b8f686ee78529cde6b2704df367e33b9308
Akash Shyam <akash.shyam2008@gmail.com>
2021-09-29 09:39:46 +0530

    chore(env): update env

--- global.d.ts
+++ global.d.ts
@@ -1,11 +1,11 @@
 namespace NodeJS {
-	interface ProcessEnv {
-		PORT?: number;
-		MONGO_URI: string;
-		JWT_SECRET: string;
-		CLOUDINARY_API_KEY: string;
-		GOOGLE_AUTH_API_KEY: string;
-		CLOUDINARY_API_SECRET: string;
-		NODE_ENV: 'development' | 'production';
-	}
+  interface ProcessEnv {
+    PORT?: number;
+    MONGO_URI: string;
+    JWT_SECRET: string;
+    CLOUDINARY_API_KEY: string;
+    GOOGLE_AUTH_API_KEY: string;
+    CLOUDINARY_API_SECRET: string;
+    NODE_ENV: 'development' | 'production' | 'test';
+  }
 }
